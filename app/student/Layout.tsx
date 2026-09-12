42a440da71a1c5d7800b48e64f5ebebbf7ac6b73
Melanie1-1dev <melaniendikubwimana@gmail.com>
2026-09-12 18:29:53 +0300

    Updated

--- app/student/Layout.tsx
+++ app/student/Layout.tsx
@@ -1,20 +1,16 @@
-import Sidebar from "../../components/Sidebar";
+import TeacherSidebar from "../../components/TeacherSidebar";
 import Navbar from "../../components/Navbar";
 
-export default function StudentLayout({
+export default function TeacherLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
     <div className="flex min-h-screen bg-gray-50 dark:bg-gray-900 transition-colors">
-      {/* Sidebar */}
-      <Sidebar />
-
-      {/* Main Content Area */}
+      <TeacherSidebar />
       <div className="flex-1 flex flex-col">
         <Navbar />
-        
         <main className="flex-1 p-6 overflow-y-auto">
           {children}
         </main>
